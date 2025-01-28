ed7938e91c95281f7b3bf1d714cd9593e1b1084a
Max Barnes <115467575+MaxBarnes68@users.noreply.github.com>
2025-01-28 10:09:29 +0000

    Delete Phase4EndProjectWorkspace/phase4endprojectpetclinic directory

--- Phase4EndProjectWorkspace/phase4endprojectpetclinic/src/main/java/com/project/phase4endprojectpetclinic/repo/OwnerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.phase4endprojectpetclinic.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.phase4endprojectpetclinic.entity.Owner;
-
-public interface OwnerRepository  extends JpaRepository<Owner, Long> {
-
-}
--- Phase4EndProjectWorkspace/phase4endprojectpetclinic/src/main/java/com/project/phase4endprojectpetclinic/repo/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.phase4endprojectpetclinic.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.phase4endprojectpetclinic.entity.Pet;
-
-public interface PetRepository  extends JpaRepository<Pet, Long>  {
-
-}
